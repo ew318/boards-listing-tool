57895a5324b2cd012a44411c3f2f32ab84b6ce24
Emily Waters <emily22waters@hotmail.co.uk>
2025-02-02 11:54:56 +0000

    Adding happy path test and freezing requirements - total 3 hours

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,18 +3,14 @@ from pathlib import Path
 
 from boards_listing_tool.app import (
     check_file_boards,
+    join_board_data,
     load_json_file,
     validate_board_schema,
 )
 
-#############
-# TEST PLAN #
-#############
-
-# Test Cases
+# TODO Test Cases
 #   No directory set
 #   Finds all the json files in directory (nested etc)
-#   Invalid json in .json file
 
 
 def test_check_file_boards():
@@ -72,3 +68,38 @@ def test_load_json_file():
     # Json file
     valid_json_path = os.path.join(cwd, "tests", "data", "valid.json")
     assert load_json_file(Path(valid_json_path)) != None
+
+
+def test_whole_happy_path(mocker):
+    cwd = Path.cwd()
+    expected_data = {
+        "boards": [
+            {
+                "name": "B7-400X",
+                "vendor": "Boards R Us",
+                "core": "Cortex-M7",
+                "has_wifi": True,
+            },
+            {
+                "name": "D4-200S",
+                "vendor": "Boards R Us",
+                "core": "Cortex-M4",
+                "has_wifi": False,
+            },
+            {
+                "name": "Low_Power",
+                "vendor": "Tech Corp.",
+                "core": "Cortex-M0+",
+                "has_wifi": False,
+            },
+        ],
+        "_metadata": {"total_vendors": 2, "total_boards": 3},
+    }
+    get_json_file_paths_mock = mocker.patch(
+        "boards_listing_tool.app.get_json_file_paths"
+    )
+    boards1 = Path(os.path.join(cwd, "data", "boards-1.json"))
+    boards2 = Path(os.path.join(cwd, "data", "boards-2.json"))
+    get_json_file_paths_mock.return_value = [boards1, boards2]
+    actual_data = join_board_data()
+    assert actual_data == expected_data
